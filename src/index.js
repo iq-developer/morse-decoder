59bd84b8a2163a87b0a23decc0862c10e6779f23
Vladimir Sevastianov <108.nimay@gmail.com>
2020-10-23 20:49:51 +0300

    feat: basic functions implement

--- src/index.js
+++ src/index.js
@@ -38,9 +38,46 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+    function digitizer(dotsAndDashes) {
+        let  result = '';
+        for (let char of dotsAndDashes) {
+                if (char == '.') {
+                    result += '10';
+                } else if (char == '-') {
+                    result += '11';
+                }
+        }
+        for (let i = result.length; i < 10; i++) {
+            result = '0' + result;
+        }
+        return result;
+    }
+
+    function InvertObject(obj) {
+        for (key in obj){
+            this[obj[key]] = digitizer(key);
+        }
+    }
+
+    const MORSE_TABLE_DECODER = new InvertObject(MORSE_TABLE);
+
+    let  final = '';
+    for (char of expr){
+        final += MORSE_TABLE_DECODER[char];
+    }
+
+
+    //console.log(MORSE_TABLE_DECODER['m']);
+
+    //console.log(digitizer('--'));
+
+return final;
+
 }
 
-module.exports = {
-    decode
-}
+console.log( decode('me') );
+
+// module.exports = {
+//     decode
+// }
